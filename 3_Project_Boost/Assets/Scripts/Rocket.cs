64cccb3061c731be0d037f77cf1b3ab039357015
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2018-01-06 17:15:14 -0600

    Basic Input
    -Built out primitives placeholder rocket
    -added script for basic control imput

---
+++ 3_Project_Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+
+    }
+}
