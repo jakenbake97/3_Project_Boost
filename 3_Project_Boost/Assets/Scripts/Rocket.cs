15f74199c154aaddd1bf2e62d35f5110e290eca7
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2018-01-07 12:46:53 -0600

    Better Control and Tagging
    Added better ship control and a way to change settings in editor. Added Friendly and fuel tags and a collision detection in the console.

--- 3_Project_Boost/Assets/Scripts/Rocket.cs
+++ 3_Project_Boost/Assets/Scripts/Rocket.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
-    public GameObject rudder1;
-    public GameObject rudder2;
     AudioSource audioSource;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
 
     // Use this for initialization
     void Start()
@@ -20,14 +20,48 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Rotate();
+        Thrust();
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("dead");
+                break;
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; // take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidbody.freezeRotation = false; // resume physics control of rotation 
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying) //prevents audio from layering
             {
                 audioSource.Play();
@@ -37,18 +71,5 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward);
-            rudder1.transform.Rotate(-Vector3.forward * 0.3f);
-            rudder2.transform.Rotate(-Vector3.forward * 0.3f);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
-            rudder1.transform.Rotate(Vector3.forward * 0.3f);
-            rudder2.transform.Rotate(Vector3.forward * 0.3f);
-        }
-
     }
 }
