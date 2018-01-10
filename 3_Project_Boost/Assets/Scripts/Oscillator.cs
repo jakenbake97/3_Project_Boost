8a39b9e69c90722503492ec4f5a64a9a0eae5d24
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2018-01-09 18:25:53 -0600

    Level 3 and osscilation
    Added level 3 and created the osscilation script to control a moving object

---
+++ 3_Project_Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    /*TODO: 
+	*	remove movementFactor from inspector
+	*/
+    [SerializeField] Vector3 movementVector;
+
+    //TODO: remove from inspector later
+    [Range(0, 1)]
+    [SerializeField]
+    float movementFactor; // 0 for not moved, 1 for fully moved
+
+    Vector3 startingPos;
+
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
